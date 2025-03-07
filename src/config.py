efec88ff9a39a73466e4268aaeaaf562d49093d3
Keleseth <shasa.keles@gmail.com>
2025-03-07 13:34:46 +0300

    pre-commit configured

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 import os
-from dotenv import load_dotenv
 
+from dotenv import load_dotenv
 from pydantic import ConfigDict, SecretStr
 from pydantic_settings import BaseSettings
 
--- src/database/db_dependencies.py
+++ src/database/db_dependencies.py
@@ -5,7 +5,6 @@ from sqlalchemy.orm import sessionmaker
 
 from src.config import settings
 
-
 engine = create_async_engine(settings.database_url)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession)
 
