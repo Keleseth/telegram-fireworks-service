f84d2c6a8d6ef14604bad1fc20a0fd6722a2ce28
Evgn22 <rezonans2352@mail.ru>
2025-03-08 02:03:09 +0900

    add coma and orginized imports in models/newsletter

--- src/models/newsletter.py
+++ src/models/newsletter.py
@@ -1,24 +1,24 @@
 from typing import List
 
+from database import Base
 from sqlalchemy import DateTime, ForeignKey, Integer, Text, text
 from sqlalchemy.orm import Mapped, mapped_column
-from database import Base
 
 
 class Newsletter(Base):
     id: Mapped[int] = mapped_column(
-        Integer, primary_key=True, autoincrement=True
+        Integer, primary_key=True, autoincrement=True,
     )
     content: Mapped[Text]
     datetime_send: Mapped[DateTime]
     switch_send: Mapped[bool] = mapped_column(
-        default=False, server_default=text("'false'")
+        default=False, server_default=text("'false'"),
         )
 
 
 class NewsletterMedia(Base):
     id: Mapped[int] = mapped_column(
-        Integer, primary_key=True, autoincrement=True
+        Integer, primary_key=True, autoincrement=True,
     )
     newsletter_id: Mapped[int] = mapped_column(ForeignKey('newsletters.id'))
     media_url: Mapped[List[str]]
