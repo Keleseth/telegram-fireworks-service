204a8d9ebb32d77208ddc11a80f5f52a5d14a2ae
Савва Великородов <savvavelikorodov@mail.com>
2025-03-10 10:20:37 +0300

    main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,16 @@
-# Это основной запускаемый файл.
-# Заменить содержимое своим кодом.
+from fastapi import FastAPI
 
-import sys
+from src.api.v1.router import main_router
 
-print(sys.path)
+# from app.core.config import settings
+# from app.core.init_db import create_first_superuser
+
+# app = FastAPI(title=settings.app_title)
+app = FastAPI()
+
+app.include_router(main_router)
+
+
+# @app.on_event('startup')
+# async def startup():
+#     await create_first_superuser()
