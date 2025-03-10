d2dbead5b1dcde915c7216a2b491d28173c4428f
Савва Великородов <savvavelikorodov@mail.com>
2025-03-10 10:30:04 +0300

    title

--- src/main.py
+++ src/main.py
@@ -1,16 +1,17 @@
 from fastapi import FastAPI
 
 from src.api.v1.router import main_router
+from src.config import settings
 
-# from app.core.config import settings
-# from app.core.init_db import create_first_superuser
-
-# app = FastAPI(title=settings.app_title)
-app = FastAPI()
+app = FastAPI(title=settings.app_title, description=settings.description)
 
 app.include_router(main_router)
 
 
-# @app.on_event('startup')
-# async def startup():
-#     await create_first_superuser()
+def main():
+    """Функция запустит управляющую функцию. Для доступа извне."""
+    pass
+
+
+if __name__ == 'main':
+    main()
