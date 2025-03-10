ae64b085442687d5e286661d573e52f95bfc600e
Keleseth <shasa.keles@gmail.com>
2025-03-10 15:58:18 +0300

    blank endpoints with docstrings and explanations

--- src/api/v1/endpoints/cart.py
+++ src/api/v1/endpoints/cart.py
@@ -1,3 +1,62 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status
+from pydantic import BaseModel
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.database.db_dependencies import get_async_session
 
 router = APIRouter()
+
+
+@router.post(
+    '/user/cart',
+    status_code=status.HTTP_201_CREATED,
+    response_model=dict[str, str],
+)
+async def add_product_to_cart(
+    create_data: BaseModel,  # Создать свою схему.
+    session: AsyncSession = Depends(get_async_session),
+) -> dict[str, str]:
+    """Добавить продукт в корзину.
+
+    Доступен age_verified пользователям.
+    """
+    # TODO: Нужно в схему для энпоинта добавить поле telegram_id,
+    # чтобы эндпоинт в теле запроса знал о пользователе.
+    # НЕ добавлять telegram_id в path или query параметры.
+    return {'message': 'Запрос выполнен успешно!'}
+
+
+@router.get(
+    '/user/cart',
+    status_code=status.HTTP_200_OK,
+    response_model=dict[str, str],
+)
+async def get_user_cart(
+    session: AsyncSession = Depends(get_async_session),
+) -> dict[str, str]:
+    """Получить содержимое корзины пользователя.
+
+    Доступен age_verified пользователям.
+    """
+    # TODO: Пагинация.
+    # TODO: Необходимо получить telegram_id из тела запроса.
+    # НЕ добавлять telegram_id в path или query параметры.
+    return {'message': 'Запрос выполнен успешно!'}
+
+
+@router.delete(
+    '/user/cart/{firework_id}',
+    status_code=status.HTTP_204_NO_CONTENT,
+    response_model=dict[str, str],
+)
+async def delete_product_from_cart(
+    firework_id: int,
+    session: AsyncSession = Depends(get_async_session),
+) -> dict[str, str]:
+    """Удалить продукт из корзины пользователя.
+
+    Доступен age_verified пользователям.
+    """
+    # TODO: Необходимо получить telegram_id из тела запроса.
+    # НЕ добавлять telegram_id в path или query параметры.
+    return {'message': 'Запрос выполнен успешно!'}
--- src/api/v1/endpoints/product.py
+++ src/api/v1/endpoints/product.py
@@ -1,3 +1,56 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.database.db_dependencies import get_async_session
 
 router = APIRouter()
+
+
+@router.get(
+    '/сategories',
+    status_code=status.HTTP_200_OK,
+    response_model=dict[str, str],
+)
+async def get_сategories(
+    session: AsyncSession = Depends(get_async_session),
+) -> dict[str, str]:
+    """Получить все категории фейерверков.
+
+    Доступен всем пользователям.
+    """
+    # TODO: Добвавить пагинацию до 10 категорий за раз.
+    return {'message': 'Запрос выполнен успешно!'}
+
+
+@router.get(
+    '/fireworks',
+    status_code=status.HTTP_200_OK,
+    response_model=dict[str, str],
+)
+async def get_fireworks(
+    session: AsyncSession = Depends(get_async_session),
+) -> dict[str, str]:
+    """Получить фейерверки.
+
+    Доступен всем пользователям.
+    """
+    # TODO: Добавить фильтрацию по тегам, категориям,
+    # параметрам, избранным и ценам.
+    # TODO: Пагинация.
+    return {'message': 'Запрос выполнен успешно!'}
+
+
+@router.get(
+    'fireworks/{firework_id}',
+    status_code=status.HTTP_200_OK,
+    response_model=dict[str, str],
+)
+async def get_firework_by_id(
+    firework_id: int,
+    session: AsyncSession = Depends(get_async_session),
+) -> dict[str, str]:
+    """Получить продукт фейерверк по id.
+
+    Доступен всем пользователям.
+    """
+    return {'message': 'Запрос выполнен успешно!'}
