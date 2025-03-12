babbbcbc7984484d7ddeb14df84b0665e758b3e4
Keleseth <shasa.keles@gmail.com>
2025-03-12 10:00:37 +0300

    fastapi-users 14.0.1 - get_user_manager() takes 2 arguments only

--- src/api/auth/dependencies.py
+++ src/api/auth/dependencies.py
@@ -2,16 +2,16 @@ from fastapi_users import FastAPIUsers
 
 from src.api.auth.auth import auth_backend
 from src.api.auth.manager import get_user_manager
-from src.models.user import User
-from src.schemas.user import UserCreate, UserRead, UserUpdate
+
+# from src.schemas.user import UserCreate, UserRead, UserUpdate
 
 fastapi_users = FastAPIUsers(
     get_user_manager,
     [auth_backend],
-    User,
-    UserCreate,
-    UserUpdate,
-    UserRead,
+    # User,
+    # UserCreate,
+    # UserUpdate,
+    # UserRead,
 )
 
 current_user = fastapi_users.current_user(active=True)
