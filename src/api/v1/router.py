b0ae98c9e1945d6bba34887b4eaab6dfa49266c1
Vadim P <ya@vadzrf.ru>
2025-03-14 20:16:03 +0300

    Сохранение текущих изменений перед мержем

--- src/api/v1/router.py
+++ src/api/v1/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
-from src.api.v1.endpoints.product import router
+from src.api.v1.endpoints import product_router
 
 main_router = APIRouter()
 
-main_router.include_router(router)
+main_router.include_router(product_router)
