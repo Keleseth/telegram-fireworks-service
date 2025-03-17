08d3bc9a851dfcffaa9af141475f0d2d71a7586b
Keleseth <shasa.keles@gmail.com>
2025-03-17 11:57:40 +0300

    filter_schema updates

--- src/schemas/filter_shema.py
+++ src/schemas/filter_shema.py
@@ -9,19 +9,27 @@ class FireworkFilterSchema(BaseModel):
     """Схема для фильтрации фейеверков."""
 
     name: Optional[str] = Field(
-        None, title='Фильтр по совпадению с именем фейеверка.'
+        default=None, title='Фильтр по совпадению с именем фейеверка.'
     )
     number_of_volleys: Optional[int] = Field(
-        None, title='Фильтр по количеству залпов.'
+        default=None, title='Фильтр по количеству залпов.'
     )
     categories: Optional[List[str]] = Field(
-        None, title='Фильтр по категориям.'
+        default=None, title='Фильтр по категориям.'
     )
     # sub_categories: Optional[List[str]] = Field(
     #     None, title='Фильтр по подкатегориям.'
     # )
-    article: Optional[str] = Field(None, title='Фильтр по артикулу')
-    tags: Optional[List[str]] = Field(None, title='Список фильтров по тегам.')
-    min_price: Optional[int] = Field(None, title='Минимальная цена.')
-    max_price: Optional[int] = Field(None, title='Максимальная цена.')
-    order_by: Optional[List[str]] = Field(None, title='Сортировка по полям.')
+    article: Optional[str] = Field(default=None, title='Фильтр по артикулу')
+    tags: Optional[List[str]] = Field(
+        default=None, title='Список фильтров по тегам.'
+    )
+    min_price: Optional[int] = Field(default=None, title='Минимальная цена.')
+    max_price: Optional[int] = Field(default=None, title='Максимальная цена.')
+    order_by: Optional[List[str]] = Field(
+        ['name'], title='Сортировка по полям.'
+    )
+
+    model_config = {
+        'extra': 'ignore',
+    }
