1077c9bc677acc84808676012e7eee1b36c1dc5f
Sergey Varyukhov <s1zeist@gmail.com>
2025-03-22 17:01:54 +0300

    fix clear_db

--- src/service/clear_db.py
+++ src/service/clear_db.py
@@ -27,7 +27,7 @@ async def clear_tables(session: AsyncSession) -> None:
             "favoritefirework",
             "firework_tag",
             "fireworkdiscount",
-            "firework_property",  # Добавляем новую таблицу
+            "firework_property",
             "firework",
             "category",
             "tag"
