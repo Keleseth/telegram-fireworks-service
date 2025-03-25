ce2c8ce8927dffb02e63174273fcaac459ec7d2e
Keleseth <shasa.keles@gmail.com>
2025-03-25 18:24:22 +0300

    custom templates changes

--- src/admin/promotion_admin.py
+++ src/admin/promotion_admin.py
@@ -8,6 +8,7 @@ class DiscountView(ModelView, model=Discount):
 
     name = 'акция'
     name_plural = 'Акции'
+    list_template = 'sqladmin/custom_list.html'
 
     column_list = [
         Discount.id,
@@ -16,9 +17,24 @@ class DiscountView(ModelView, model=Discount):
         Discount.description,
         Discount.start_date,
         Discount.end_date,
+        Discount.fireworks,
     ]
-
     form_excluded_columns = [
         'created_at',
         'updated_at',
     ]
+    column_details_exclude_list = [
+        'hashed_password',
+        'favorite_fireworks',
+        'cart',
+        'is_verified',
+    ]
+    column_labels = {
+        'id': 'ID',
+        'type': 'тип акции',
+        'value': 'значение скидки',
+        'start_date': 'дата и время начала акции',
+        'end_date': 'дата и время окончания акции',
+        'description': 'описание',
+        'fireworks': 'акционные фейерверки',
+    }
