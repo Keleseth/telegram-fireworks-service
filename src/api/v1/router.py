74d185b0a5df1deb645c803800462f38b559ce56
Keleseth <shasa.keles@gmail.com>
2025-03-25 16:45:51 +0300

    migration tested. tests were passed

--- src/api/v1/router.py
+++ src/api/v1/router.py
@@ -4,6 +4,7 @@ from src.api.v1.endpoints import (
     address_router,
     cart_router,
     discount_router,
+    favorite_router,
     order_router,
     product_router,
     user_router,
@@ -17,3 +18,4 @@ main_router.include_router(discount_router, tags=['Акции'])
 main_router.include_router(address_router, tags=['Адреса'])
 main_router.include_router(cart_router, tags=['Корзина'])
 main_router.include_router(user_router, tags=['Пользователи'])
+main_router.include_router(favorite_router, tags=['Избранные'])
