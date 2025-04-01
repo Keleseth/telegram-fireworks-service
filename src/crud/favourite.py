44480b7d69cc0612f26dcb7ad1e780b0d53f46d7
Stepan Gerasimov <mr.gerasimov.stepan@yandex.ru>
2025-04-02 01:04:11 +0300

    fixed

--- src/crud/favourite.py
+++ src/crud/favourite.py
@@ -8,8 +8,8 @@ from sqlalchemy.orm import joinedload, selectinload
 
 from src.models.base import BaseJFModel
 from src.models.favorite import FavoriteFirework
-from src.models.user import User
 from src.schemas.favourite import FavoriteCreate
+from src.models.product import Firework
 
 ModelType = TypeVar('ModelType', bound=BaseJFModel)
 
@@ -25,23 +25,6 @@ class CRUDFavourite:
         """
         self.model = model
 
-    async def get_user_id_by_telegram_id(
-        self,
-        obj_in: FavoriteCreate,
-        session: AsyncSession,
-    ):
-        """Метод для получения user_id по telegram_id."""
-        obj_in = obj_in.dict()
-        return UUID(
-            (
-                await session.execute(
-                    select(User.id).where(
-                        User.telegram_id == obj_in['telegram_id']
-                    )
-                )
-            ).scalar_one_or_none()
-        )
-
     async def create_favourite_by_telegram_id(
         self,
         obj_in: FavoriteCreate,
@@ -50,10 +33,31 @@ class CRUDFavourite:
     ):
         """Метод для добавления фейерверка в избранное по telegram_id."""
         obj_in_data = obj_in.dict()
-        if user_id:
-            db_obj = self.model(
-                user_id=user_id, firework_id=obj_in_data['firework_id']
+        existing = await session.execute(
+            select(self.model).where(
+                self.model.user_id == user_id,
+                self.model.firework_id == obj_in_data['firework_id']
             )
+        )
+        if existing.scalar_one_or_none():
+            raise HTTPException(
+                status_code=409,
+                detail="Этот фейерверк уже добавлен в избранное"
+            )
+        firework = await session.execute(
+            select(Firework).where(
+                id == obj_in_data['firework_id']
+            )
+        )
+        if not firework.scalar_one_or_none():
+            raise HTTPException(
+                status_code=404,
+                detail="Такого фейерверка не существует"
+            )
+        db_obj = self.model(
+            user_id=user_id,
+            firework_id=obj_in_data['firework_id']
+        )
         session.add(db_obj)
         await session.commit()
         await session.refresh(db_obj)
@@ -65,7 +69,6 @@ class CRUDFavourite:
         session: AsyncSession,
     ):
         """Метод для получения избранных по telegram_id."""
-        # user_id = UUID("fb310c59-ace1-46cc-a3e4-3b073f47ee45")
         query = (
             select(self.model)
             .options(joinedload(self.model.firework))
@@ -84,7 +87,7 @@ class CRUDFavourite:
         """Метод для удаления избранных по telegram_id."""
         db_obj = await session.scalar(
             select(self.model)
-            .options(selectinload(self.model.firework))  # Загружаем `firework`
+            .options(selectinload(self.model.firework))
             .where(
                 self.model.user_id == user_id,
                 self.model.firework_id == firework_id,
